1a6a5fb3be1e5c3756e7f1db86c1952cc6b9572a
Angel Añez <angdavid2910@gmail.com>
2024-06-04 12:05:52 -0400

    version 1.1. Fixed background

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ function App() {
   return (
     <main
       className="bg-gradient-to-b from-blue-night to-slate-900
-     min-h-screen h-full text-white flex flex-col justify-center"
+     min-h-screen text-white flex flex-col justify-center"
     >
-      <div className="snowflake opacity-70 h-full min-h-screen animate-falling absolute w-full"></div>
-      <Card />
+      <div className="snowflake min-h-screen animate-falling w-full flex flex-col">
+        <div className="min-h-screen flex flex-col w-full bg-gradient-to-b from-[rgba(13,33,112,0.3)] to-[rgba(2,6,20,0.3)] justify-center">
+          <Card />
+        </div>
+      </div>
     </main>
   );
 }
