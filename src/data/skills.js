dbc3cc306f7d98f410adfdf3db37ddd102b40073
Angel Añez <angdavid2910@gmail.com>
2024-06-27 12:00:42 -0400

    Learned Docker

--- src/data/skills.js
+++ src/data/skills.js
@@ -6,7 +6,8 @@ const skills = [
     {"name": "NodeJs", "icon": "./nodejs.svg", "learned-at": 21},
     {"name": "MySQL", "icon": "./mysql.svg", "learned-at": 22},
     {"name": "TailwindCSS", "icon": "./tailwindcss.svg", "learned-at": 22},
-    {"name": "React", "icon": "./react.svg", "learned-at": 22}
+    {"name": "React", "icon": "./react.svg", "learned-at": 22},
+    {"name": "Docker", "icon": "./docker.svg", "learned-at": 22}
 ]
 
 export default skills;
